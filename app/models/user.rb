b20f5ea06e2cc6db69e14f0c1733cbee51713046
Danylo Udalov <gtafan51@gmail.com>
2019-02-14 18:28:53 +0000

    Add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false },
+                       length: { minimum: 6, maximum: 25 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
+                    length: { maximum: 60 }, format: { with: VALID_EMAIL_REGEX }
   enum role: [:user, :moderator, :admin]
 end
