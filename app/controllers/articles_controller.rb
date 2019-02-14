8ea1e650a6b894bb3ef4b813f365f5fbd50e22b7
Danylo Udalov <gtafan51@gmail.com>
2019-02-14 07:41:07 +0000

    Add edit and update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,25 +1,36 @@
 class ArticlesController < ApplicationController
+  before_action :article_resource, only: %i[show edit update destroy]
+
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Article #{ @article.title } was successuful created!"
+      flash[:notice] = "Article #{@article.title} was successuful created!"
       redirect_to article_path(@article)
     else
       render :new
     end
   end
 
-  def show
-    @article = Article.find(params[:id])
+  def update
+    if @article.update(article_params)
+      flash[:notice] = "Article #{@article.title} was successuful updated!"
+      redirect_to article_path(@article)
+    else
+      render :edit
+    end
   end
 
   private
-  
+
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def article_resource
+    @article = Article.find(params[:id])
   end
 end
