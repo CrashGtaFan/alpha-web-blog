b67af21bafca8863c1ce175bfcd3f7b0166ee5c2
Danylo Udalov <gtafan51@gmail.com>
2019-02-15 07:21:57 +0000

    Add user registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'pages/about'
+  get 'signup', to: 'users#new'
 
   root to: 'pages#welcome'
   resources :articles
+  resources :users, except: [:new]
 end
